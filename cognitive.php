5680fbb832bbceff0c67acfea7cbd232b86b7f7a
Adwi Arifin <adwiarifin@gmail.com>
2019-04-06 23:56:23 +0700

    add bootstrap

--- cognitive.php
+++ cognitive.php
@@ -15,20 +15,14 @@ $connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUN
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 $containerName = "adwicontainer";
 try {
-    echo "These are the blobs present in the container: ";
-
-    echo "<table>";
-    echo "<thead>";
-    echo "  <tr>";
-    echo "    <th>Name</th>";
-    echo "    <th>Action</th>";
-    echo "  </tr>";
-    echo "</thead>";
-    echo "<tbody>";
+    $data = array();
     do{
         $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
         foreach ($result->getBlobs() as $blob) {
-            echo "<tr><td><a href=\"".$blob->getUrl()."\">".$blob->getName()."</a></td></tr>";
+            $data[] = array(
+                'name' => $blob->getName(),
+                'url' => $blob->getUrl()
+            );
         }
     
         $listBlobsOptions->setContinuationToken($result->getContinuationToken());
@@ -49,3 +43,43 @@ try {
     $error_message = $e->getMessage();
     echo $code.": ".$error_message."<br />";
 }
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Hello, Bootstrap Table!</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.14.2/dist/bootstrap-table.min.css">
+  </head>
+  <body>
+    <table data-toggle="table">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Image</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php $no = 1; foreach($data as $blob): ?>
+        <tr>
+          <td><?php echo $no++; ?></td>
+          <td><img src="<?php echo $blob['url']; ?>" alt="<?php echo $blob['name']; ?>" width="200"></td>
+          <td><button>Analyze</button></td>
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.14.2/dist/bootstrap-table.min.js"></script>
+  </body>
+</html>
