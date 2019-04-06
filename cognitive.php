6b07906ea4cd9506744fb3e48a2d05eb80840e02
Adwi Arifin <adwiarifin@gmail.com>
2019-04-07 02:42:22 +0700

    add error handling

--- cognitive.php
+++ cognitive.php
@@ -48,10 +48,26 @@ try {
 }
 
 if (isset($_POST['submit']) && $_POST['submit'] == 'Upload') {
-    //Upload blob
-    $fileToUpload = $_FILES["image"]["name"];
-    $content      = fopen($_FILES["image"]["tmp_name"], "r");
-    $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+    try {
+        //Upload blob
+        $fileToUpload = $_FILES["image"]["name"];
+        $content      = fopen($_FILES["image"]["tmp_name"], "r");
+        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+    } catch(ServiceException $e) {
+        // Handle exception based on error codes and messages.
+        // Error codes and messages are here:
+        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code.": ".$error_message."<br />";
+    } catch(InvalidArgumentTypeException $e) {
+        // Handle exception based on error codes and messages.
+        // Error codes and messages are here:
+        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code.": ".$error_message."<br />";
+    }
 }
 
 if (isset($_POST['submit']) && $_POST['submit'] == 'Analyze') {
